7ed56bc99d14c2f860b52ca8479165a5df4717ed
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-09-30 04:25:49 -0300

    Project created

---
+++ backend/src/test/java/com/rlbpc/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlbpc.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

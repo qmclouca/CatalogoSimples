67e9e406500e02e03ac250eac9b1928910668906
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-10-21 10:39:57 -0300

    Category search by id
    A new end point responsible by implements search of categories by id was
    implemented

--- backend/src/main/java/com/rlbpc/catalogo/resources/CategoryResource.java
+++ backend/src/main/java/com/rlbpc/catalogo/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +21,17 @@ public class CategoryResource {
 	@Autowired
 	private CategoryService service;
 	
+	//End-Point para buscar todas as categorias
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	//End-Point para buscar as categorias por id	
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/rlbpc/catalogo/services/CategoryService.java
+++ backend/src/main/java/com/rlbpc/catalogo/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.rlbpc.catalogo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +35,12 @@ public class CategoryService {
 		//implementação usando funções de alta ordem		
 		List<Category> list = repository.findAll(); 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 }

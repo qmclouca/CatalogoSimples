2f001e9c67e73848d8176683ab4e6fe3ab6d43ad
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-10-26 10:22:52 -0300

    Start point to front-end

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import Alert from './Alert';
-
 const App = () => {
     return (
-      <div className = "container mt-5">
-        <Alert text="Rodolfo"/>
-        <Alert text="Bortoluzzi"/>
-        <Alert />
-      </div>  
+      <h1>App</h1>  
     );
 }
 

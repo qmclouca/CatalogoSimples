bed310af47752bba9be2522177f2ddf7e674fc9e
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-10-27 20:37:08 -0300

    Design HOME, styles

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import './styles.scss';
+import {ReactComponent as MainImage } from '../../core/assets/images/main-image.svg'
 
 const Home = () => (
-    <div>
-        <h1>Home</h1>
+    <div className = "home-container">
+        <div className="row home-content">
+            <div className="col-6">
+                <h1 className="text-title"> 
+                    Conheça o melhor <br /> catálogo de produtos
+                </h1>
+                <p className="text-subtitle">
+                  Ajudaremos você a encontrar os melhores <br />
+                  produtos do mercado.
+                </p>
+            </div>
+            <div className="col-6">
+                <MainImage className = "main-image" />
+            </div>
+        </div>
     </div>
 );
 export default Home;

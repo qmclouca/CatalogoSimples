7c18a7da1455fbc03dabb77222f8839bd43adcce
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-10-27 19:42:15 -0300

    Routes working

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './core/assets/styles/custom.scss';
 import './App.scss';
 import Navbar from './core/assets/components/Navbar';
+import Routes from './Routes';
 
 const App = () => {
     return (
-      <Navbar/>
+      <Routes />
     );
 }
 

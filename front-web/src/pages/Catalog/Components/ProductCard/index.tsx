a630577f9c29f4e2db925164df13c8adc978663d
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-10-29 10:16:13 -0300

    Product catalog, product details, product price

--- front-web/src/pages/Catalog/Components/ProductCard/index.tsx
+++ front-web/src/pages/Catalog/Components/ProductCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.scss';
 import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg';
+import ProductPrice from '../../../../core/components/ProductPrice';
 
 
 const ProductCard = () => (
@@ -10,10 +11,7 @@ const ProductCard = () => (
            <h6 className = "product-name"> 
                 Computador Desktop - Intel Core i7
            </h6>
-           <div className = "product-price-container">
-               <span className = "product-currency">R$</span>
-                <h3 className = "product-price">2.779,00</h3>
-           </div>
+           <ProductPrice price="2.779,00"/>
        </div>
     </div>
 );
--- front-web/src/pages/Catalog/Components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/Components/ProductDetails/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg';
+import ProductPrice from '../../../../core/components/ProductPrice';
 import './styles.scss';
 
 type ParamsType = {
@@ -12,7 +15,37 @@ const ProductDetails = () => {
 
     return (
         <div className = "product-details-container">
-            <h1>Product details</h1>
+            <div className = "card-base border-radius-20 product-details">
+               <Link to="/products" className = "products-details-goback">
+                    <ArrowIcon className = "icon-goback"/>
+                    <h1 className = "text-goback">voltar</h1>
+               </Link>
+               <div className = "row">
+                   <div className = "col-6 pr-5">
+                       <div className = "product-details-card text-center">
+                            <ProductImage className = "product-details-image" />
+                       </div>
+                       <h1 className = "product-details-name">
+                           Computador Desktop - Intel Core i7
+                       </h1>
+                       <ProductPrice price="3.779,00"/>
+                   </div>
+                   <div className = "col-6 product-details-card">
+                        <h1 className = "product-description-title"> 
+                            Descrição do produto 
+                        </h1>
+                        <h1 className = "product-description-text">
+                            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br />XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br />
+                            kfjewofjweoifjwoiejfoihjweofhew<br />qorgopqbergoibrogboiqrhgoihqrwgohwoirhgwiohr<br />
+                            rwgoiwhrgiohqwroighqwopireghwqo<br />iperhgowiqehgoiwqheogihwoieghowiqhegoihwqe<br />
+                            hwqoeghoiweqhgoiwqehgoihwqeogihw<br />qeopghwoqiehgowihegoiwhqegohweoighwoiehgoiw<br />
+                            woiehboiwebhgwbqheouigboreghoiewhr<br />oíqwhergoiwhregoi´hwergoíwebgfoiwqefhbhiwoqef<br />
+                            werogibwnoiergbwoqierhgoiwhgioh<br />woieghwoiehgfoiwqhegoíwqhegoiwhbqeoighwoiehggewwó<br />
+                            jwebgowqbeoigbwoiegbowieghoweh<br />gfoiwhegoihwqeoighwqoíegho´wqiehgoi´whegoihoihwegwq<br />
+                        </h1>
+                   </div>
+               </div>
+            </div>
         </div>
     );
 };

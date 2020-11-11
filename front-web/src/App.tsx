4ab878f32b06f7ef0ecab204d64acb163c9389f1
Rodolfo Lucas Bortoluzzi <qmclouca@gmail.com>
2020-11-10 21:25:54 -0300

    manipulating form data with ReactJS

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'core/assets/styles/custom.scss';
 import './App.scss';
-import Navbar from './core/components/Navbar';
 import Routes from './Routes';
 
 const App = () => {
--- front-web/src/pages/Admin/Components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Components/Products/Form/index.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
 const Form = () => {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('computadores');
+
+    
+    const handleOnChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+
+    const handleOnChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPrice(event.target.value);
+    }
+
+    const handleOnChangeCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setCategory(event.target.value);
+    }
     return (
         <BaseForm title="CADASTRAR UM PRODUTO">
+            <h1 className="my-5">
+               Nome: {name} <br/>
+               Preço: {price} <br/>
+               Categoria: {category}
+            </h1>
             <div className = "row">
                 <div className="col-6">
-                    <input type="text" className="form-control"/>
+                    <input 
+                    value={name}
+                    type="text" 
+                    className="form-control mb-5"
+                    onChange={handleOnChangeName}
+                    placeholder="Nome do Produto"
+                    />
+                    <select value={category} className="form-control mb-5" onChange={handleOnChangeCategory}>
+                        <option value="livros">Livros</option>
+                        <option value="computadores">Computadores</option>
+                        <option value="eletrônicos">Eletrônicos</option>
+                    </select>
+                    <input 
+                    value={price}
+                    type="text" 
+                    className="form-control"
+                    onChange={handleOnChangePrice}
+                    placeholder="Preço"
+                    />
                 </div>
             </div>
         </BaseForm>

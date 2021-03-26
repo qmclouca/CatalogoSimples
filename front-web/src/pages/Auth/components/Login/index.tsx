cf3f5c8a42b407d9760701b69a4614c30087e345
qmclouca <qmclouca@gmail.com>
2021-03-26 11:42:34 -0300

    Using react-hook-form to manage form data!

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,22 +1,41 @@
-import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import ButtonIcon from 'core/components/ButtonIcon';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    password: string;
+}
+
+
+
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (data:FormData) => {
+        console.log(data);
+        //chamar a API de autenticação
+    }
+
     return(
         <AuthCard title="login">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                <input 
                  type="email" 
                  className="form-control input-base margin-bottom-30"
                  placeholder="Email"
+                 name="email"
+                 ref={register}
                 />
                 <input 
                  type="password" 
                  className="form-control input-base"
                  placeholder="Senha"
+                 name="password"
+                 ref={register}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">Recuperar a senha!</Link>
                 <div className="login-submit">

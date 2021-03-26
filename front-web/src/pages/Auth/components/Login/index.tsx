99d024570a20a3dcc0b583dacdfb69ab54ef576a
qmclouca <qmclouca@gmail.com>
2021-03-26 11:13:55 -0300

    Adding login form

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,4 +1,6 @@
+import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
 
@@ -6,7 +8,28 @@ const Login = () => {
     return(
         <AuthCard title="login">
             <form className="login-form">
-                <h1> Formulário de login</h1>
+               <input 
+                 type="email" 
+                 className="form-control input-base margin-bottom-30"
+                 placeholder="Email"
+                />
+                <input 
+                 type="password" 
+                 className="form-control input-base"
+                 placeholder="Senha"
+                />
+                <Link to="/admin/auth/recover" className="login-link-recover">Recuperar a senha!</Link>
+                <div className="login-submit">
+                    <ButtonIcon text="logar" />
+                </div>
+                <div className="text-center">
+                    <span className="not-registered">
+                        Não possui cadastro?
+                    </span>
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
             </form>
         </AuthCard>
 

95a9e655e31fd87eca5504a4be41751fe1d9e535
qmclouca <qmclouca@gmail.com>
2021-03-31 11:51:36 -0300

    Product form validations using React Hook Form

--- front-web/src/pages/Admin/Components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Components/Products/Form/index.tsx
@@ -12,7 +12,7 @@ type FormState = {
 }
 
 const Form = () => {
-    const { register, handleSubmit } = useForm<FormState>();
+    const { register, handleSubmit, errors } = useForm<FormState>();
         
     const onSubmit = (data: FormState) => {
         makePrivateRequest({url:'/products', method:'POST', data});
@@ -24,37 +24,71 @@ const Form = () => {
             
             <div className = "row">
                 <div className="col-6">
-                    <input 
-                    ref={register({ required: "Campo obrigatório"})}
-                    name="name"
-                    type="text" 
-                    className="form-control margin-bottom-30 input-base"
-                    placeholder="Nome do Produto"
-                    />
-                    <input 
-                    ref={register({ required: "Campo obrigatório"})}
-                    name="price"
-                    type="number" 
-                    className="form-control margin-bottom-30 input-base"
-                    placeholder="Preço"
-                    />
-                    <input 
-                    ref={register({ required: "Campo obrigatório"})}
-                    name="imageUrl"
-                    type="text" 
-                    className="form-control margin-bottom-30 input-base"
-                    placeholder="Imagem do produto"
-                    />
+                    <div className="margin-bottom-30">
+                        <input 
+                          ref={register({ 
+                              required: "Campo obrigatório",
+                              minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
+                              maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres'}
+
+                            })}
+                          name="name"
+                          type="text" 
+                          className="form-control input-base"
+                          placeholder="Nome do Produto"
+                        />
+                        {errors.name && (
+                            <div className="invalid-feedback d-block">
+                                {errors.name.message}
+                            </div> 
+                        )}
+                    </div>
+
+                    <div className="margin-bottom-30">
+                        <input 
+                          ref={register({ required: "Campo obrigatório"})}
+                          name="price"
+                          type="number" 
+                          className="form-control input-base"
+                          placeholder="Preço"
+                        />
+                        {errors.price && (
+                            <div className="invalid-feedback d-block">
+                                {errors.price.message}
+                            </div> 
+                        )}
+                    </div>
+
+                    <div className="margin-bottom-30">
+                        <input 
+                          ref={register({ required: "Campo obrigatório"})}
+                          name="imageUrl"
+                          type="text" 
+                          className="form-control input-base"
+                          placeholder="Imagem do produto"
+                        />
+                        {errors.imageUrl && (
+                            <div className="invalid-feedback d-block">
+                                {errors.imageUrl.message}
+                            </div> 
+                        )}
+                    </div>
                 </div>
+
                 <div className="col-6">
                     <textarea  
-                        ref={register({ required: "Campo obrigatório"})}
-                        name="description"
-                        className="form-control input-base"
-                        placeholder="Descrição"
-                        cols={30} 
-                        rows={10} 
+                      ref={register({ required: "Campo obrigatório"})}
+                      name="description"
+                      className="form-control input-base"
+                      placeholder="Descrição"
+                      cols={30} 
+                      rows={10} 
                     />
+                    {errors.description && (
+                            <div className="invalid-feedback d-block">
+                                {errors.description.message}
+                            </div> 
+                        )}
                 </div>
             </div>
         </BaseForm>

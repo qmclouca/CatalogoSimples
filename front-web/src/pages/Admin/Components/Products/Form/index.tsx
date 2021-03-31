6ef234db5f29c7a0eeb3908aafac8811187c144d
rodolfo <qmclouca@gmail.com>
2021-03-31 16:50:47 -0300

    Add confirmation toast when saving products

--- front-web/src/pages/Admin/Components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Components/Products/Form/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 import { useForm } from 'react-hook-form';
+import {toast} from 'react-toastify';
 import { makePrivateRequest } from 'core/utils/request';
+import { useHistory } from 'react-router-dom';
 
 type FormState = {
     name: string;
@@ -13,9 +15,18 @@ type FormState = {
 
 const Form = () => {
     const { register, handleSubmit, errors } = useForm<FormState>();
-        
+    const history = useHistory();
+
+
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({url:'/products', method:'POST', data});
+        makePrivateRequest({url:'/products', method:'POST', data})
+        .then(() => {
+            toast.info('Produto salvo com sucesso!');
+            history.push('admin/products');
+        })
+        .catch(() => {
+            toast.error('Erro ao salvar produto!');
+        })
     }
 
     return (

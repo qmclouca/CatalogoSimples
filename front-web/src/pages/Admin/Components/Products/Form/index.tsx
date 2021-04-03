b2dfbe67158184f0e481e5c7610d473a4c410af8
qmclouca <qmclouca@gmail.com>
2021-04-03 17:27:36 -0300

    Add categories on product creation

--- front-web/src/pages/Admin/Components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Components/Products/Form/index.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import {toast} from 'react-toastify';
+import Select from 'react-select';
 import { makePrivateRequest, makeRequest } from 'core/utils/request';
 import { useHistory, useParams } from 'react-router-dom';
+import { Category } from 'core/types/Products';
 
 type FormState = {
     name: string;
     price: string;
     description: string;
     imgUrl: string;
+    categories:Category[];
 }
 
 type ParamsType = {
@@ -18,9 +21,11 @@ type ParamsType = {
 }
 
 const Form = () => {
-    const { register, handleSubmit, errors, setValue } = useForm<FormState>();
+    const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
     const history = useHistory();
     const { productId } = useParams<ParamsType>();
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+    const [categories, setCategories] = useState<Category[]>([]);
     const isEditing = productId !== 'create';
     const formTitle = isEditing ? 'Editar produto' : 'Cadastrar um produto';
 
@@ -32,9 +37,18 @@ const Form = () => {
                     setValue('price', response.data.price);
                     setValue('description', response.data.description);
                     setValue('imgUrl', response.data.imgUrl);
+                    setValue('categories', response.data.categories);
                 })
         }
     }, [productId, isEditing, setValue]);
+
+    useEffect(() => {
+        setIsLoadingCategories(true);
+        makeRequest({url: `/categories`})
+            .then(response => setCategories(response.data.content))
+            .finally(() => setIsLoadingCategories(false));
+    },[]);
+
 
     const onSubmit = (data: FormState) => {
         makePrivateRequest({
@@ -55,8 +69,7 @@ const Form = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
         <BaseForm 
             title= {formTitle}        
-        >
-            
+        >  
             <div className = "row">
                 <div className="col-6">
                     <div className="margin-bottom-30">
@@ -78,6 +91,25 @@ const Form = () => {
                             </div> 
                         )}
                     </div>
+                <div className = "margin-bottom-30">
+                    <Controller
+                        as = {Select}
+                        name = "categories"
+                        rules = {{required:true}}
+                        control = {control} 
+                        isLoading = {isLoadingCategories}                        
+                        options = {categories}
+                        getOptionLabel = {(option: Category) => option.name}
+                        getOptionValue = {(option: Category) => String(option.id)}
+                        classNamePrefix = "categories-select"
+                        placeholder = "Categorias"
+                        isMulti    
+                    />{errors.categories && (
+                        <div className="invalid-feedback d-block">
+                            Campo obrigatório
+                        </div> 
+                    )}
+                </div>
 
                     <div className="margin-bottom-30">
                         <input 
@@ -114,7 +146,7 @@ const Form = () => {
                     <textarea  
                       ref={register({ required: "Campo obrigatório"})}
                       name="description"
-                      className="form-control input-base"
+                      className="form-control input-base-description"
                       placeholder="Descrição"
                       cols={30} 
                       rows={10} 

1afd2fe3b13c8080ca8d82b2fea6f91f4142a9c0
qmclouca <qmclouca@gmail.com>
2021-04-04 10:16:27 -0300

    AWS S3 upload dependencies included in POM file

--- backend/src/main/java/com/rlbpc/catalogo/entities/User.java
+++ backend/src/main/java/com/rlbpc/catalogo/entities/User.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.persistence.Column;

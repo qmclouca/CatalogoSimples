6ada3eab7be8fe5ded0f75897611ad20c2735c75
qmclouca <qmclouca@gmail.com>
2021-04-04 11:26:19 -0300

    First upload test to S3 AWS service

--- backend/src/main/java/com/rlbpc/catalogo/CatalogoApplication.java
+++ backend/src/main/java/com/rlbpc/catalogo/CatalogoApplication.java
@@ -1,13 +1,27 @@
 package com.rlbpc.catalogo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CatalogoApplication {
+import com.rlbpc.catalogo.services.S3Service;
 
+@SpringBootApplication
+public class CatalogoApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("F:\\Google Drive\\RODOLFO BORTOLUZZI\\ESTUDOS\\GRADUAÇÕES\\ENGENHARIA DE SOFTWARE\\CURSO BOOTCAMP DEVSUPERIOR\\teste upload.jpg");
+		
 	}
 
 }
